3fee669350f38a1856757c6fb65f5b6cecf0e291
Eduardo Lima <dualmeidalima@gmail.com>
2020-07-02 13:35:06 -0300

    Section 1: First Spec Implementation - CalculatorService

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,9 +1,18 @@
+import {CalculatorService} from "./calculator.service";
+import {LoggerService} from "./logger.service";
+
 describe('CalculatorService', () => {
   it('should add 2 numbers', function () {
-    pending();
+    const calculatorService = new CalculatorService(new LoggerService());
+    const result = calculatorService.add(4, 2);
+
+    expect(result).toBe(6);
   });
 
   it('should subtract 2 numbers', function () {
-    fail();
+    const calculatorService = new CalculatorService(new LoggerService());
+    const result = calculatorService.subtract(4, 2);
+
+    expect(result).toBe(2, 'Unexpected Subtraction Result');
   });
 })
