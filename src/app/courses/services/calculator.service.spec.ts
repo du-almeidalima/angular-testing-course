29af2efd0159a759f858cbb554df1fb5be24dfa7
Eduardo Lima <dualmeidalima@gmail.com>
2020-07-02 13:53:38 -0300

    Section 1: Introduction to Jasmine Spies

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,10 +3,14 @@ import {LoggerService} from "./logger.service";
 
 describe('CalculatorService', () => {
   it('should add 2 numbers', function () {
-    const calculatorService = new CalculatorService(new LoggerService());
+    const loggerService = new LoggerService();
+    spyOn(loggerService, 'log');
+    const calculatorService = new CalculatorService(loggerService);
+
     const result = calculatorService.add(4, 2);
 
     expect(result).toBe(6);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract 2 numbers', function () {
--- src/app/courses/services/logger.service.ts
+++ src/app/courses/services/logger.service.ts
@@ -7,7 +7,7 @@ import {Injectable} from '@angular/core';
 export class LoggerService {
 
   log(message:string) {
-    console.log(message);
+    console.log('Real LoggerService >> ', message);
   }
 
 }
