6c534dec83c639622f7d36a137cfbbe1359aeee1
Eduardo de Almeida Lima <eduardo.lima@venturus.org.br>
2020-07-08 14:06:57 -0300

    Section 2: Angular HTTP Service Testing

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { CoursesService } from './courses.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { COURSES } from '../../../../server/db-data';
+
 
 // Global
 let courseService: CoursesService,
@@ -26,6 +28,25 @@ describe('CoursesService', () => {
 
   it('should create', () => {
     expect(courseService).toBeTruthy();
+  });
+
+  it('should get all courses from findAllCourses()', () => {
+
+    courseService.findAllCourses().subscribe(courses => {
+      const course12 = courses.find( c => c.id === 12);
+
+      expect(courses).toBeTruthy('No courses returned from findAllCourses()');
+      expect(courses.length).toEqual(12, 'Incorrect number of Courses, COURSES array have changed?');
+      expect(course12.titles.description).toEqual('Angular Testing Course');
+    });
+
+    // Mocking/Spying on the Request
+    const req = httpTestingController.expectOne('/api/courses');
+
+    // Flushing the fake data, this will make an request and returning the data passed (Using the format expected from the request)
+    req.flush( { payload: Object.values(COURSES) } );
+
+    expect(req.request.method).toEqual('GET');
   });
 });
 
